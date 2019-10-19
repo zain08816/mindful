58e991318dcf64d83a1b65654d2c337362093b48
Kirtan Patel <kirtanpatl@gmail.com>
2019-10-19 14:18:34 -0400

    Add HTML page

--- mindful.py
+++ mindful.py
@@ -1,11 +1,10 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
