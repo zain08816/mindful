62205a4107ec83db3bfba49e43e393f3280fc977
zain08816 <zain08816@gmail.com>
2019-10-19 16:27:08 -0400

    add some changes

--- mindful.py
+++ mindful.py
@@ -1,10 +1,50 @@
 from flask import Flask, escape, request, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('home.html')
+
+level_names = {
+    0 : 'not so bruh',
+    1 : 'bruh',
+    2 : 'super bruh',
+    3 : 'ultimate bruh',
+    4 : 'Zenyatta'
+}
+
+
+exp = 0
+level = 0
+max_level = len(level_names)-1
+
+next_level_exp = level*10
+
+if level != max_level:
+    next_level_name = level_names[level+1]
+else:
+    next_level_name = 'MAX LEVEL'
+
+if (level != max_level) and (exp == next_level_exp):
+    level += 1
+
+
+
+
+frontend_package = {
+    'user_name' : '',
+    'level' : level,
+    'exp' : exp,
+    'level_name' : level_names[level],
+    'next_level_name' : next_level_name
+}
+
+frontend_json = json.dumps(frontend_package)
+
+print(frontend_json)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
