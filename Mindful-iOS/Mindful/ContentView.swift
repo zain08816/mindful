0f1676418b3394c4ab1c88e1391e2b4beb93d6bc
Kirtan Patel <kirtanpatl@gmail.com>
2019-10-19 20:53:22 -0400

    iOS

---
+++ Mindful-iOS/Mindful/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mindful
+//
+//  Created by Kirtan Patel on 10/19/19.
+//  Copyright © 2019 Kirtan Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
