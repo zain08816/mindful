918c6747fe43f1c7c388d55bb8fc571a39e789e0
Zain Ali <zain08816@gmail.com>
2019-10-19 20:46:30 -0400

    add daily_challenges

--- mindful.py
+++ mindful.py
@@ -51,18 +51,31 @@ def getLogin(username):
 
 def generateNewUserJSON( user ):
     level_names = {
-        0 : 'not so bruh',
         1 : 'bruh',
         2 : 'super bruh',
         3 : 'ultimate bruh',
         4 : 'Zenyatta'
     }
+    
 
+    
+    gold = 0
     exp = 0
-    level = 0
+    level = 1
     max_level = len(level_names)-1
 
     next_level_exp = level*10
+
+
+    bonus = 1*level
+    daily_points = bonus + 4
+    daily_goals = {
+        'Meditate for 10 minutes.' : False,
+        'Tell yourself something potitive.' : False,
+        'Excerise or take a walk.' : False,
+        'Do something you enjoy.' : False,
+        'Get 7 to 8 hrs of sleep' : False
+    }
 
     if level != max_level:
         next_level_name = level_names[level+1]
@@ -75,9 +88,14 @@ def generateNewUserJSON( user ):
     frontend_package = {
         'user_name' : user,
         'level' : level,
+        'gold' : gold,
         'exp' : exp,
         'level_name' : level_names[level],
-        'next_level_name' : next_level_name
+        'next_level_name' : next_level_name,
+        'next_level_exp' : next_level_exp,
+        'daily_goals' : daily_goals,
+        'daily_points' : daily_points,
+        'bonus' : bonus
     }
 
     frontend_json = json.dumps(frontend_package)
